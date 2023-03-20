e74b8f43161082ab87c71de07c966fb757e92390
Karel Pičman <karel.picman@kontron.com>
2023-03-20 13:31:56 +0100

    Javascript comments

--- rpr-login.php
+++ rpr-login.php
@@ -936,10 +936,10 @@ if ( !class_exists( 'RPR_Login' ) ) {
                             var shortPass = 1, badPass = 2, goodPass = 3, strongPass = 4, mismatch = 5, symbolSize = 0, natLog, score;
                             // password 1 !== password 2
                             if (password1 !== password2 && password2.length > 0)
-                                return mismatch
-                            // password < <?php echo absint( $register_plus_redux->rpr_get_option( 'min_password_length' ) ); ?>
+                                return mismatch;
+                            /* password < <?php echo absint( $register_plus_redux->rpr_get_option( 'min_password_length' ) ); ?> */
                             if (password1.length < <?php echo absint( $register_plus_redux->rpr_get_option( 'min_password_length' ) ); ?>)
-                                return shortPass
+                                return shortPass;
                             // password1 === username
                             if (password1.toLowerCase() === username.toLowerCase())
                                 return badPass;
@@ -951,12 +951,12 @@ if ( !class_exists( 'RPR_Login' ) ) {
                                 symbolSize +=26;
                             if (password1.match(/[^a-zA-Z0-9]/))
                                 symbolSize +=31;
-                            natLog = Math.log(Math.pow(symbolSize, password1.length));
+                                natLog = Math.log(Math.pow(symbolSize, password1.length));
                                 score = natLog / Math.LN2;
                             if (score < 40)
-                                return badPass
+                                return badPass;
                             if (score < 56)
-                                return goodPass
+                                return goodPass;
                             return strongPass;
                         }
                         jQuery(document).ready( function() {
